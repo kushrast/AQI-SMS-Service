159271915192f91ce2781b4f3bc8ccdbef3a6a72
Kush Rastogi <krustey@gmail.com>
2020-09-11 01:39:51 -0700

    Storing phone # and zip code in dynamo DB

--- request_lambda.py
+++ request_lambda.py
@@ -1,16 +1,44 @@
 from __future__ import print_function
-# from botocore.vendored import requests
-import requests
+
+# import requests
 import csv
 import os
 import re
+
+# AWS Requirements
+import boto3
+from botocore.exceptions import ClientError
+from botocore.vendored import requests
+
+dynamodb = boto3.client('dynamodb')
+
+def update_saved_info(phone_number, zip_code):
+	try:
+		response = dynamodb.get_item(TableName="aqi_db", Key={'phone_number': {'S': phone_number}})
+	except ClientError as e:
+		print(e.response['Error']['Message'])
+	else:
+		if "Item" not in response:
+			dynamodb.put_item(TableName="aqi_db", Item={'phone_number': {'S': phone_number}, 'zip_code': {'S': zip_code}})
+		elif response['Item']['zip_code'] != zip_code:
+			dynamodb.update_item(TableName="aqi_db", Key={'phone_number': {'S': phone_number}}, UpdateExpression = "set zip_code = :z", ExpressionAttributeValues={":z": {"S": zip_code}})
+
+def check_if_phone_number_saved(phone_number):
+	try:
+		response = dynamodb.get_item(TableName="aqi_db", Key={'phone_number': {'S': phone_number}})
+	except ClientError as e:
+		print(e.response['Error']['Message'])
+	else:
+		if "Item" in response:
+			return response["Item"]
+
 
 def get_AirNow_api_key():
 	return os.environ['airnow_api_key']
 
 def get_AQI(zip_code):
 	if is_valid_zip_code(zip_code):
-		response = requests.get('http://www.airnowapi.org/aq/observation/zipCode/current/?format=text/csv&zipCode=94568&distance=10&API_KEY=%s' % (get_AirNow_api_key()))
+		response = requests.get('http://www.airnowapi.org/aq/observation/zipCode/current/?format=text/csv&zipCode=%s&distance=10&API_KEY=%s' % (zip_code, get_AirNow_api_key()))
 
 		response_body = response.text
 		csv_reader = csv.DictReader(response_body.splitlines())
@@ -31,10 +59,14 @@ def is_valid_zip_code(zip_code):
 	return regex.match(zip_code)
 
 def lambda_handler(event, context):
-    print("Received context: " + str(context))
+    print("Received event: " + str(event))
 
     zip_code = event['Body']
+    phone_number = event['From'][3:]
     aqi = get_AQI(zip_code)
+
+    update_saved_info(phone_number, zip_code)
+
     formatted_text = format_AQI(aqi, zip_code)
     return '<?xml version=\"1.0\" encoding=\"UTF-8\"?>'\
            '<Response><Message><Body>%s</Body></Message></Response>' % (formatted_text)
